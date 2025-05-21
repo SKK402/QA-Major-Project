ce26e60fc0910c8ecd4850207ad77b215b9600c0
Som Kunar <hackerkunar@gmail.com>
2025-05-21 11:46:36 +0530

    Create implicitlyWait.java

---
+++ WaitMethods/implicitlyWait.java
@@ -0,0 +1,51 @@
+package polarisweb;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public
+
+
+class LoginPage {
+WebDriver driver;
+    
+    @BeforeMethod
+	public void openBrowser() throws InterruptedException {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+	}
+    @Test(priority=1)
+    public void youtube() throws InterruptedException {
+		driver.get("https://www.xbox.com/en-IN/");
+		
+		WebElement inputbox = driver.findElement(By.xpath("//*[@id=\"ContentBlockList_3\"]/div/div/a[2]/div/p/span"));
+		
+		inputbox.click();
+		
+		driver.findElement(By.xpath("//*[text() = \"Minecraft: Java & Bedrock Edition for PC\"]")).click();
+		
+        }
+		
+    }
+    
+    @AfterMethod
+    public void closeBrowser() throws InterruptedException {
+    	Thread.sleep(3000);
+    	driver.quit();
+    }
+    
+    
+}
